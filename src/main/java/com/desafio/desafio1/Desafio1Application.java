5e3e791d960bf6e9172c9c4be4d571130d9af1bb
guiferrini <guilhermemferrini@gmail.com>
2024-09-11 14:57:55 -0300

    teste

---
+++ src/main/java/com/desafio/desafio1/Desafio1Application.java
@@ -0,0 +1,20 @@
+package com.desafio.desafio1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("teste");
+	}
+
+}
